25ce2c826eb61bd7d10ddd1fb48d4b8b4c84f964
mthorson <mthorson@usatoday.com>
2015-01-02 10:52:34 -0500

    adding files

---
+++ js/app.js
@@ -0,0 +1,217 @@
+var app = app || {};
+
+_.extend(app, {
+  models: {},
+  collections: {},
+  views: {}
+});
+
+app.models.Question = Backbone.Model.extend({
+  defaults: {
+    highlight: false
+  },
+
+  initialize: function(){
+    this.attributes.category = this.attributes.category.split(", ");
+  }
+});
+
+
+// Questions Collection
+// ---------------
+
+// The collection of questions is backed by json file
+app.collections.Questions = Backbone.Collection.extend({
+
+  // Reference to this collection's model.
+  model: app.models.Question,
+  url: "js/data.json"
+
+
+});
+// Create our global collection of **questions**.
+app.collections.questions = new app.collections.Questions();
+// app.collections.questions.bind("reset", _.once(Backbone.history.start));
+
+// App-wide View
+// ----
+
+app.views.AppView = Backbone.View.extend({
+  el: "body",
+  events: {
+    "click .modal-overlay": "removeHighlight",
+    // "touchstart .modal-overlay": "removeHighlight" 
+  },
+
+  initialize: function() {
+    this.listenTo(app.collections.questions, 'reset', this.addAll);
+    app.collections.questions.fetch({reset: true});
+  },
+
+  addOne: function(question) {
+
+    var view = new app.views.QuestionCard({model: question});
+    $("#card-wrap").append(view.render().el);
+  },
+
+  addAll: function() {
+
+    Backbone.history.start();
+    app.collections.questions.each(this.addOne, this);
+    this.addTimeStamp();
+  },
+
+  removeHighlight: function() {
+    Analytics.click("closed card");
+    app.views.detailView.model.set({"highlight": false});
+  },
+  addTimeStamp: function() {
+      var objData = app.collections.questions.toJSON();
+      this.$el.find(".time-stamp").html(objData[0].timestamp);
+  }
+});
+
+// QuestionCard View
+// ----
+
+app.views.QuestionCard = Backbone.View.extend({
+  tagName: "div",
+
+  className: "card small-card",
+
+  events: {
+    "click": "setHighlight"
+  },
+
+  template: app.templates["CARD-FRONT.HTML"],
+
+  initialize: function() {
+    this.listenTo(this.model, 'change', this.showDetail);
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+
+
+
+    _.each(this.model.attributes.category, function(v, i) {
+      this.$el.addClass(v);
+      this.$el.attr( 'data-category', v);
+    }, this);
+
+    return this;
+  },
+
+  setHighlight: function() {
+    Analytics.click("opened card");
+    this.model.set({"highlight": true});
+  },
+
+  showDetail: function() {
+
+    if(this.model.get("highlight")) {
+      app.views.detailView = new app.views.DetailCard({model: this.model});
+
+      $(".page-wrap").append(app.views.detailView.render().el);
+      app.views.detailView.postRender(app.views.detailView.render().$el);
+    }
+
+    
+
+  }
+});
+
+
+app.views.DetailCard = Backbone.View.extend({
+  tagName: "div",
+  className: "modal",
+  template: app.templates["CARD-BACK.HTML"],
+
+  events: {
+    "click .close-card": "removeHighlight",
+    // "touchstart .close-card": "removeHighlight",
+   
+  },
+
+  initialize: function() {
+
+    app.router.navigate("questions/" + this.model.get("rowNumber"));
+    this.listenTo(this.model, 'change', this.removeCard);
+  },
+  render: function() {
+    this.$el.empty();
+    
+    this.$el.html(this.template(this.model.attributes));   
+    return this;
+  },
+
+  postRender: function(element) {
+
+    _.defer(function() {
+      $(".modal-overlay").addClass("show");
+
+      element.addClass("modal-show");
+    }, element);
+      
+  },
+
+  removeCard: function() {
+    
+    if(!this.model.get("highlight")) {
+      
+      $(".modal-overlay").removeClass("show");
+      this.$el.removeClass("modal-show");
+      _.defer(function() { app.router.navigate("questions"); });
+      this.stopListening();
+    }
+    
+  },
+
+  removeHighlight: function() {
+    this.model.set({"highlight": false});
+  }
+
+});
+
+
+app.Router = Backbone.Router.extend({
+
+  routes: {
+    "": "home",
+    "questions/:id":                 "highlight",    // #/1
+    
+  },
+
+  home: function() {
+
+     var highlightModel = _.find(app.collections.questions.models, function(model) {
+      
+      return model.get("highlight") === true;
+    });
+     if(highlightModel) {
+      highlightModel.set({"highlight": false});
+     }
+     
+  },
+
+  highlight: function(id) {
+    var detailModel = _.find(app.collections.questions.models, function(model) {
+      
+      return model.get("rowNumber") == id;
+    });
+    detailModel.set({"highlight": true});
+    app.views.detailView = new app.views.DetailCard({model: detailModel});
+
+      $(".page-wrap").append(app.views.detailView.render().el);
+      app.views.detailView.postRender(app.views.detailView.render().$el);
+  }
+
+});
+
+app.router = new app.Router();
+
+
+$(document).ready(function() {
+ app.views.appView = new app.views.AppView();
+ 
+});
